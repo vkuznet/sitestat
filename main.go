9f87f4a7a531ef5e8b69a2b7ef8a0c6d8c36825f
Valentin Kuznetsov <vkuznet@gmail.com>
2016-02-17 13:33:52 -0500

    New options

--- main.go
+++ main.go
@@ -17,15 +17,21 @@ func main() {
 	flag.StringVar(&tier, "tier", "", "Look-up specific data-tier")
 	var metric string
 	flag.StringVar(&metric, "metric", "NACC", "Popularity DB metric (NACC, TOTCPU, NUSERS)")
+	var dbsinfo bool
+	flag.BoolVar(&dbsinfo, "dbsinfo", false, "Use DBS to collect dataset information, default use PhEDEx")
 	var breakdown string
 	flag.StringVar(&breakdown, "breakdown", "", "Breakdown report into more details (tier, dataset)")
 	var bins string
 	flag.StringVar(&bins, "bins", "", "Comma separated list of bin values, e.g. 0,1,2,3,4 for naccesses or 0,10,100 for tot cpu metrics")
-	var report string
-	flag.StringVar(&report, "report", "txt", "Report type, txt or json")
+	var format string
+	flag.StringVar(&format, "format", "txt", "Output format type, txt or json")
 	var verbose int
 	flag.IntVar(&verbose, "verbose", 0, "Verbose level, support 0,1,2")
+	var profile bool
+	flag.BoolVar(&profile, "profile", false, "profile code")
 	flag.Parse()
 	utils.VERBOSE = verbose
-	cms.Process(metric, site, trange, tier, breakdown, bins, report)
+	utils.PROFILE = profile
+	cms.DBSINFO = dbsinfo
+	cms.Process(metric, site, trange, tier, breakdown, bins, format)
 }
