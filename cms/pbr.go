11e80e30fb4bd8be92604ce7de1de2ed237b4057
Valentin Kuznetsov <vkuznet@gmail.com>
2016-09-25 14:05:31 +0800

    Code clean-up; switch to use PBRMAP

--- cms/pbr.go
+++ cms/pbr.go
@@ -1,12 +1,13 @@
 // PBR module, see https://github.com/aurimasrep/PhedexReplicaMonitoring
 // Copyright (c) 2015-2016 - Valentin Kuznetsov <vkuznet@gmail.com>
-// SQLite: https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/05.3.html
+//
+// References:
 // database/sql http://go-database-sql.org/index.html
+// SQLite: https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/05.3.html
 package cms
 
 import (
 	"database/sql"
-	//     "fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/vkuznet/sitestat/utils"
 )
@@ -24,28 +25,25 @@ func (p *PBR) Map() map[string]float64 {
 	defer db.Close()
 	err = db.Ping()
 	checkErr(err)
-	// query
 	rows, err := db.Query("SELECT dataset,size FROM avg where phgroup = ?", PHGROUP)
 	checkErr(err)
 	defer rows.Close()
 
-	pbrmap := make(map[string]float64)
+	p.records = make(map[string]float64)
 	for rows.Next() {
 		var dataset string
 		var size float64
 		err = rows.Scan(&dataset, &size)
 		checkErr(err)
-		pbrmap[dataset] = size
+		p.records[dataset] = size
 	}
-	p.records = pbrmap
 	return p.records
 }
 
 func sizeFromPBR(dataset, phgroup string) float64 {
-	// query
 	rows, err := PDB.Query("SELECT dataset,size FROM avg where dataset = ? and phgroup = ?", dataset, phgroup)
 	checkErr(err)
-	//     defer rows.Close()
+	defer rows.Close()
 
 	var size float64
 	for rows.Next() {
@@ -63,12 +61,10 @@ func checkErr(err error) {
 }
 
 func datasetInfoPBR(dataset string, ch chan Record) {
-	//     var pbr PBR
-	//     pbrmap := pbr.Map()
 	rec := make(Record)
 	rec["name"] = dataset
-	//     rec["size"] = pbrmap[dataset]
-	rec["size"] = sizeFromPBR(dataset, PHGROUP)
+	rec["size"] = PBRMAP[dataset]
+	//     rec["size"] = sizeFromPBR(dataset, PHGROUP)
 	rec["tier"] = utils.DataTier(dataset)
 	ch <- rec
 }
